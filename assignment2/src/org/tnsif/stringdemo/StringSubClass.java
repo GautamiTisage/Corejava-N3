4daa0139d8d87fd651daa4168201905f0334c64b
gauta <gauta@LAPTOP12>
2023-02-21 00:55:18 +0530

    assignment 2

---
+++ assignment2/src/org/tnsif/stringdemo/StringSubClass.java
@@ -0,0 +1,10 @@
+package org.tnsif.stringdemo;
+
+public class StringSubClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ assignment2/src/wrappereclassfdemo/wrapperclassdemo.java
@@ -0,0 +1,20 @@
+package wrappereclassfdemo;
+
+public class wrapperclassdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Autoboxing
+				int x=18;
+				Integer y=x;
+				System.out.println(y);
+				
+				//UnBoxing
+				Integer i=new Integer(10);
+				System.out.println(i);
+				int b=i.intValue();
+				System.out.println(b);
+
+	}
+
+}
